923c35da4d401bf208334cf15aacff7e462b19e1
streitsandor <sandorstreit@gmail.com>
2017-04-21 08:58:54 +0200

    GraphViz 1.0

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Izé";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(479, 30);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(479, 403);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 433);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GraphVizWrapper;
+using GraphVizWrapper.Commands;
+using GraphVizWrapper.Queries;
+using System.IO;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String gyujto(Ember peldany, int szint)
+        {
+            if (peldany.Bejarva)
+                return "";
+
+            peldany.Bejarva = true;
+
+            StringBuilder sb = new StringBuilder();
+
+            if (peldany.parja != null)
+            {
+                sb.Append(peldany.Nev);
+                sb.Append("->");
+                sb.Append(peldany.parja.Nev);
+                sb.Append(";");
+
+                sb.Append(gyujto(peldany.parja, szint));
+            }
+
+            foreach (Ember e in peldany.gyermekek)
+            {
+                sb.Append(peldany.Nev);
+                sb.Append("->");
+                sb.Append(e.Nev);
+                sb.Append("[penwidth =" + szint * 2 + ", color = \"" + ColorTranslator.ToHtml(peldany.Szín) + "\"];");
+                sb.Append(gyujto(e, szint + 1));
+            }
+
+            return sb.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ember Apa = new Ember("Apa");
+            Ember Anya = new Ember("Anya");
+
+            Ember gy1 = new Ember("Gy1", Color.Red);
+            Ember gy2 = new Ember("Gy2");
+
+            Ember u1 = new Ember("U1");
+
+            Ember uu1 = new Ember("UU1");
+            Ember uu1parja = new Ember("UU1parja");
+
+            Ember uuu1 = new Ember("UUU1");
+            Ember uuu2 = new Ember("UUU2");
+
+            Apa.parja = Anya;
+            Anya.parja = Apa;
+            uu1.parja = uu1parja;
+            uu1parja.parja = uu1;
+
+            Apa.gyermekek.Add(gy1);
+            Apa.gyermekek.Add(gy2);
+            Anya.gyermekek.Add(gy1);
+            Anya.gyermekek.Add(gy2);
+
+            gy1.gyermekek.Add(u1);
+
+            u1.gyermekek.Add(uu1);
+
+            uu1parja.gyermekek.Add(uuu1);
+            uu1parja.gyermekek.Add(uuu2);
+
+            String s = gyujto(Apa, 1);
+
+            var getStartProcessQuery = new GetStartProcessQuery();
+            var getProcessStartInfoQuery = new GetProcessStartInfoQuery();
+            var registerLayoutPluginCommand = new RegisterLayoutPluginCommand(getProcessStartInfoQuery, getStartProcessQuery);
+
+            var wrapper = new GraphGeneration(getStartProcessQuery,
+                                              getProcessStartInfoQuery,
+                                              registerLayoutPluginCommand);
+
+            byte[] output = wrapper.GenerateGraph("digraph{" + s + "}", Enums.GraphReturnType.Png);
+
+            using (MemoryStream ms = new MemoryStream(output))
+            {
+                Image i = Image.FromStream(ms);
+                pictureBox1.Image = i;
+            }
+        }
+    }
+}
